93123c6cf551e2950ed08d370ed05206b669580d
Nguyên Dương <102743853+nguyengz@users.noreply.github.com>
2023-07-19 17:13:55 +0700

    new

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const SERICE_API = process.env.REACT_APP_SERVICE_API;
-const API_URL = "file/category/list";
+const API_URL = "/file/category/list";
 
 const fetchCategory = () => {
   return axios.get(SERICE_API + API_URL);
